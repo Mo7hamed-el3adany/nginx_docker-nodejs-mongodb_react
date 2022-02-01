ae092d5a120fc4390abb2a64a5d87ee6d28d8d2c
El_Sadany32 <mohammedelsadany32@gmail.com>
2022-02-01 03:37:15 +0200

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react';
+
+const App = () => {
+  useEffect(() => {
+    fetch('/api/products')
+    .then(res => res.json())
+    .then(data => console.log(data))
+  },[])
+  return <div>
+    <h1>Hello Client Edit</h1>
+    <h1>Viet Nam Vo Dich!</h1>
+    <h1>Viet Nam Vo Dich!</h1>
+    <h1>Viet Nam Vo Dich!</h1>
+  </div>;
+};
+
+export default App;
